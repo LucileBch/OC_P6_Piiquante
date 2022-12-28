d99e4c6b34a4574908721a034af7a4d94a20c5e8
LucileBch <l.bouchire@gmail.com>
2022-12-28 20:43:45 +0100

    initialisation de l'appli Express

--- backend/app.js
+++ backend/app.js
@@ -4,5 +4,15 @@ const express = require('express');
 // Création d'une application express
 const app = express();
 
+//Middleware test
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+  });
+
 // Exportation de l'application pour l'exploiter à partir d'autres fichiers
 module.exports = app;
