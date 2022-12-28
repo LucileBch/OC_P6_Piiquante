48e03a457434795fb0bd80ae2f4cf644d9160581
LucileBch <l.bouchire@gmail.com>
2022-12-28 20:36:29 +0100

    installation d'Express

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+// Importation d'express
+const express = require('express');
+
+// Création d'une application express
+const app = express();
+
+// Exportation de l'application pour l'exploiter à partir d'autres fichiers
+module.exports = app;
