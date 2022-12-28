7884c01ee189a468a209ed2cf2504fa15f8e08f1
LucileBch <l.bouchire@gmail.com>
2022-12-28 20:49:42 +0100

    connexion à mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
-// Importation d'express
+// Importation d'express et de mongoose
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Création d'une application express
 const app = express();
+
+// Connexion de l'API à la base de données
+mongoose.connect('mongodb+srv://LucileBch:4987@cluster0.kbrdrtp.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 //Middleware test
 app.use((req, res, next) => {
