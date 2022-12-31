66557ad77b2bce29d8b79803156678f48ea8ce99
LucileBch <l.bouchire@gmail.com>
2022-12-31 15:34:48 +0100

    Ajout fonction suppression sauce

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -11,7 +11,7 @@ const router = express.Router();
 // Création des routes
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-//router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauce);
 
