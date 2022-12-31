f65688ca24a80c1964393901f625bde386d845d0
LucileBch <l.bouchire@gmail.com>
2022-12-31 13:06:51 +0100

    Création modèle Sauce

---
+++ backend/models/Sauce.js
@@ -0,0 +1,21 @@
+//------------ MODELE SAUCE ------------
+// Importation de mongoose
+const mongoose = require('mongoose');
+
+// Définition du schéma de données
+const sauceSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: false, default: 0 },
+  dislikes: { type: Number, required: false, default: 0 },
+  usersLiked: { type: [String], required: false, default: [] },
+  usersDisliked: { type: [String], required: false, default: [] },
+});
+
+// Exportation de sauceSchéma avec la méthode .model
+module.exports = mongoose.model('Sauce', sauceSchema);
