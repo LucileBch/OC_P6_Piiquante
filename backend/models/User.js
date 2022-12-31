6a06f3a5192cddb788d6077d96d438a252a2f12d
LucileBch <l.bouchire@gmail.com>
2022-12-31 13:06:14 +0100

    finalisation modèle User

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,16 @@
+//------------ MODELE USER ------------
+// Importation de mongoose et de unique-validator
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+// Définition du schéma de données
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
+// Plugin pour ne pouvoir utiliser une adresse email unique pour la création de compte
 userSchema.plugin(uniqueValidator);
 
+// Exportation de sauceSchéma avec la méthode .model
 module.exports = mongoose.model('User', userSchema);
