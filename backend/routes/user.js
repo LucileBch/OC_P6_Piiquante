f72dc1fa232752cbdc9273c7999fed39fd3f8848
LucileBch <l.bouchire@gmail.com>
2023-01-03 18:03:09 +0100

    Ajout sécurité contrainte format password

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,14 @@
 //------------ LOGIQUE ROUTING USER ------------
-// Importation d'express et du controleur user
+// Importation d'express, du controleur user et du middleware password
 const express = require('express');
 const userCtrl = require('../controllers/user');
+const password = require('../middleware/password');
 
 // Création du router avec la méthode Router
 const router = express.Router();
 
-// Création des routes 
-router.post('/signup', userCtrl.signup);
+// Création des routes
+router.post('/signup', password, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 // Exportation du router
